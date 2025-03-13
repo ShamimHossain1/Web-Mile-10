34a891a60bdc295113771ed71513e3c387021660
ShamimHossain1 <shamimweb78@gmail.com>
2025-03-13 03:22:18 -0400

    phone server

--- my-first-server/index.js
+++ my-first-server/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const phones = require('./phones.json');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello Worlddddddddddddd!')
 });
 
 app.get('/about', (req, res) => {
-  res.send('About')
+    res.send('About')
+});
+
+app.get('/phones', (req, res) => {
+    res.send(phones)
+});
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone)
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Phone server is running on ${port}`)
 });
