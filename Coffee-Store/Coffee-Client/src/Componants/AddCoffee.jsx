497e194f398491c6f6c98418b164a1421b1b19ea
ShamimHossain1 <shamimweb78@gmail.com>
2025-03-22 04:13:40 -0400

    mongodb setup

---
+++ Coffee-Store/Coffee-Client/src/Componants/AddCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h1>This is add coffee</h1>
+        </div>
+    );
+};
+
+export default AddCoffee;
--- Coffee-Store/Coffee-Client/src/main.jsx
+++ Coffee-Store/Coffee-Client/src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import UpdateCoffee from './Componants/UpdateCoffee.jsx';
+import AddCoffee from './Componants/AddCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/addcoffee",
+    element: <AddCoffee></AddCoffee>,
+  },
+  {
+    path: "/updatecoffee",
+    element: <UpdateCoffee></UpdateCoffee>,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </StrictMode>,
 )
