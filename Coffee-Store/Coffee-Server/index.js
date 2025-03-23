7829fa57922e30bfbfd703dd817320cc2c951d9e
ShamimHossain1 <shamimweb78@gmail.com>
2025-03-23 01:19:45 -0400

    get all user

--- Coffee-Store/Coffee-Server/index.js
+++ Coffee-Store/Coffee-Server/index.js
@@ -81,6 +81,15 @@ async function run() {
         }
         )
 
+        // Users API
+
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+
+        });
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user);
